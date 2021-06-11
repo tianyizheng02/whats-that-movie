63f28dd8c9dfa1d2465d6bfa3682da3b751c3b77
Tianyi Zheng <tianyizheng02@gmail.com>
2021-06-11 09:06:08 -0700

    Initialize boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Info from './components/Info';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="What's That Movie?"></Header>
+      <Info></Info>
+      <Footer title="2021"></Footer>
     </div>
   );
 }
---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Info.css';
+
+function Info(props) {
+  return (
+    <div className="info">
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default Info;
