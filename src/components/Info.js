a10e7551f9220509a28942d675514088beda4ce4
Tianyi Zheng <tianyizheng02@gmail.com>
2021-06-11 11:41:13 -0700

    Implement basic search functionality

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Info.css';
+import Movies from './Movies';
 
-function Info(props) {
+function Info() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+  const [showMovies, setShowMovies] = useState(false);
+
+  function submit(e) {
+    e.preventDefault();
+    async function fetchMyAPI() {
+      const searchParam = encodeURIComponent(query);
+      const apiUrl = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}`;
+      console.log(apiUrl);
+      let response = await fetch(apiUrl);
+      response = await response.json();
+      console.log(response.Search);
+      setMovies(response.Search);
+    }
+    fetchMyAPI();
+    setShowMovies(true);
+    setQuery("");
+  }
+
   return (
     <div className="info">
-      <p>Test</p>
+      <form onSubmit={submit}>
+        <label htmlFor="queryInput">Search Movie Name:</label>
+        <input
+          id="queryInput"
+          value={query}
+          type="text"
+          onChange={e => setQuery(e.target.value)}/>
+        <button className="search">Submit</button>
+      </form>
+      {showMovies ? <Movies movies={movies}></Movies> : <></>}
     </div>
   );
 }
