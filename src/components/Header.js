8e535538fa751e4549092b9985e73a4549a135b1
Tianyi Zheng <tianyizheng02@gmail.com>
2021-06-20 04:59:08 -0700

    Add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.css';
+import Icon from '../icon.png';
 
 function Header(props) {
   return (
     <div className="header">
+      <img id="icon" src={Icon} alt="icon"/>
       <h1>What's That Movie?</h1>
     </div>
   );
